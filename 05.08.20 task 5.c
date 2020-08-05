0a4654dc11668d3be7d44bab7e56fb5222720ed5
Farrukh Davlatov <69100562+FarrukhDavlatov@users.noreply.github.com>
2020-08-05 11:28:09 +0500

    Add files via upload

---
+++ 05.08.20 task 5.c
@@ -0,0 +1,18 @@
+
+#include  <stdio.h>
+
+int main() {
+	
+	int a, b;
+	
+	printf("Write integer number:");
+	scanf("%d%d", &a, &b);
+	
+	a -= b;
+	b += a;
+	a += b;
+	
+	printf("%d", usersNumber);
+
+	return 0;
+}
